5293a408160f1f9e0446755b1eaf48c41dcc01f6
Philipp Ossler <philipp.ossler@camunda.com>
2015-09-18 11:46:52 +0200

    add test cases for script engine manager

--- src/test/scala/org/camunda/feel/ScriptEngineManagerTest.scala
+++ src/test/scala/org/camunda/feel/ScriptEngineManagerTest.scala
@@ -4,27 +4,35 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import javax.script.ScriptEngineManager
 
+import scala.collection.JavaConversions._
 
 
 /**
  * @author Philipp Ossler
  */
-class ExpressionLanguageTest extends FlatSpec with Matchers {
+class ScriptEngineManagerTest extends FlatSpec with Matchers {
   
   val scriptEngineManager = new ScriptEngineManager
     
-  "Feel script engine" should "get by name" in {
+  "Script engine manager" should "get feel script entgine by name" in {
     
     val engine = scriptEngineManager.getEngineByName("feel")
     
     engine.getClass should be (classOf[FeelScriptEngine])
   }
   
-  it should "get by extension" in {
+  it should "get feel script engine by extension" in {
    
     val engine = scriptEngineManager.getEngineByExtension("feel")
     
     engine.getClass should be (classOf[FeelScriptEngine])
+  }
+  
+  it should "contains feel script engine factotry" in {
+    
+    val factories = scriptEngineManager.getEngineFactories
+    
+    (factories map (f => f.getClass)) should contain (classOf[FeelScriptEngineFactory])
   }
   
 }
