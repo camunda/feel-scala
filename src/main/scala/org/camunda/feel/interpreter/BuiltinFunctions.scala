9941e0209dab94c3c11aef93258009a58b278c2f
Philipp Ossler <philipp.ossler@camunda.com>
2016-11-15 13:24:32 +0100

    feat(builtin): add time function

--- src/main/scala/org/camunda/feel/interpreter/BuiltinFunctions.scala
+++ src/main/scala/org/camunda/feel/interpreter/BuiltinFunctions.scala
@@ -2,6 +2,7 @@ package org.camunda.feel.interpreter
 
 import org.camunda.feel._
 import org.joda.time.LocalDate
+import org.joda.time.LocalTime
 
 /**
  * @author Philipp
@@ -14,7 +15,10 @@ object BuiltinFunctions {
 		"date" -> dateFunction,
 		"date" -> dateFunction3,
 		"date_and_time" -> dateTime,	// note that function has whitespaces in spec which will be changed in further version
-		"date_and_time" -> dateTime2
+		"date_and_time" -> dateTime2,
+		"time" -> timeFunction,
+		"time" -> timeFunction3,
+		"time" -> timeFunction4
 	)
 	
 	def dateFunction = ValFunction(List("from"), _ match {
@@ -38,6 +42,22 @@ object BuiltinFunctions {
 		case List(ValDateTime(dateTime), ValTime(time)) => ValDateTime(dateTime.toLocalDate().toLocalDateTime(time))
 		case e => error(e)
 	})	
+	
+	def timeFunction = ValFunction(List("from"), _ match {
+		case List(ValString(from)) => ValTime(from)
+		case List(ValDateTime(from)) => ValTime(from.toLocalTime())
+		case e => error(e)
+	})
+	
+	def timeFunction3 = ValFunction(List("hour", "minute", "second"), _ match {
+		case List(ValNumber(hour), ValNumber(minute), ValNumber(second)) => ValTime(new LocalTime(hour.intValue(), minute.intValue(), second.intValue()))
+		case e => error(e)
+	})
+	
+	def timeFunction4 = ValFunction(List("hour", "minute", "second", "offset"), _ match {
+		case List(ValNumber(hour), ValNumber(minute), ValNumber(second), ValDuration(offset)) => ValTime(new LocalTime(hour.intValue() + offset.getHours, minute.intValue() + offset.getMinutes, second.intValue() + offset.getSeconds))
+		case e => error(e)
+	})
 	
 	private def error(e:List[Val]) = ValError(s"illegal arguments: $e")
 	
--- src/test/scala/org/camunda/feel/interpreter/BuiltinFunctionTest.scala
+++ src/test/scala/org/camunda/feel/interpreter/BuiltinFunctionTest.scala
@@ -42,6 +42,27 @@ class BuiltinFunctionTest extends FlatSpec with Matchers {
 		eval(""" date_and_time(date and time("2012-12-24T10:24:00"),time("T23:59:00")) """) should be(ValDateTime("2012-12-24T23:59:00"))
 	}
 	
+	"A time() function" should "convert String" in {
+
+		eval(""" time("23:59:00") """) should be(ValTime("23:59:00"))
+	}
+	
+	it should "convert Date-Time" in {
+		
+		eval(""" time( date and time("2012-12-25T11:00:00") ) """) should be(ValTime("11:00:00"))
+	}
+	
+	it should "convert (hour,minute,second)" in {
+		
+		eval(""" time(23, 59, 0) """) should be(ValTime("23:59:00"))
+	}
+	
+	it should "convert (hour,minute,second, offset)" in {
+		
+		eval(""" time(14, 30, 0, duration("PT1H")) """) should be(ValTime("15:30:00"))
+	}
+	
+	
 	private def eval(expression: String, variables: Map[String, Any] = Map()): Val = {
     val exp = FeelParser.parseExpression(expression)
     interpreter.eval(exp.get)(Context(variables))
