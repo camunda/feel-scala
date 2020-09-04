bff1f71e0da531c5fee2b1464d2ecf3854907b8e
Philipp Ossler <philipp.ossler@gmail.com>
2020-09-04 14:49:09 +0200

    feat(engine): new temporal builtin functions
    * new function: "week of year()"
    * new function: "month of year()"
    * new function: "day of week()"
    * new function: "day of year()"

--- src/test/scala/org/camunda/feel/impl/builtin/BuiltinTemporalFunctionsTest.scala
+++ src/test/scala/org/camunda/feel/impl/builtin/BuiltinTemporalFunctionsTest.scala
@@ -19,7 +19,7 @@ package org.camunda.feel.impl.builtin
 import java.time.{LocalDate, LocalTime, ZoneId, ZonedDateTime}
 
 import org.camunda.feel.impl.FeelIntegrationTest
-import org.camunda.feel.syntaxtree.{ValDate, ValDateTime}
+import org.camunda.feel.syntaxtree.{ValDate, ValDateTime, ValNumber, ValString}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 class BuiltinTemporalFunctionsTest
@@ -34,6 +34,11 @@ class BuiltinTemporalFunctionsTest
     ZoneId.of("Europe/Berlin")
   )
 
+  private val date = "date(2019,9,17)"
+  private val localDateTime = """date and time("2019-09-17T14:30:00")"""
+  private val dateTime =
+    """date and time("2019-09-17T14:30:00@Europe/Berlin")"""
+
   "The now() function" should "return the current date-time" in withClock {
     clock =>
       clock.currentTime(now)
@@ -44,6 +49,43 @@ class BuiltinTemporalFunctionsTest
     clock =>
       clock.currentTime(now)
       eval(""" today() """) should be(ValDate(now.toLocalDate))
+  }
+
+  "The day of year() function" should "return the day within the year" in {
+
+    eval(s"day of year($date)") should be(ValNumber(260))
+    eval(s"day of year($localDateTime) ") should be(ValNumber(260))
+    eval(s"day of year($dateTime) ") should be(ValNumber(260))
+
+    eval(s""" day of year(date("2019-12-31")) """) should be(ValNumber(365))
+    eval(s""" day of year(date("2020-12-31")) """) should be(ValNumber(366))
+  }
+
+  "The day of week() function" should "return the day of the week" in {
+
+    eval(s"day of week($date)") should be(ValString("Tuesday"))
+    eval(s"day of week($localDateTime)") should be(ValString("Tuesday"))
+    eval(s"day of week($dateTime)") should be(ValString("Tuesday"))
+  }
+
+  "The month of year() function" should "return the month of the year" in {
+
+    eval(s"month of year($date)") should be(ValString("September"))
+    eval(s"month of year($localDateTime)") should be(ValString("September"))
+    eval(s"month of year($dateTime)") should be(ValString("September"))
+  }
+
+  "The week of year() function" should "return the number of week within the year" in {
+
+    eval(s"week of year($date)") should be(ValNumber(38))
+    eval(s"week of year($localDateTime)") should be(ValNumber(38))
+    eval(s"week of year($dateTime)") should be(ValNumber(38))
+
+    eval(""" week of year(date(2003,12,29)) """) should be(ValNumber(1))
+    eval(""" week of year(date(2004,1,4)) """) should be(ValNumber(1))
+    eval(""" week of year(date(2005,1,1)) """) should be(ValNumber(53))
+    eval(""" week of year(date(2005,1,3)) """) should be(ValNumber(1))
+    eval(""" week of year(date(2005,1,9)) """) should be(ValNumber(1))
   }
 
 }
