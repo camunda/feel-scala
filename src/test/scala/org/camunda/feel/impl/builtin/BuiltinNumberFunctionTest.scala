50e6e19b10f00b0bfeab652b91607b64724839cb
Kasper Aaquist Johansen <kasperaaquist@gmail.com>
2020-09-30 14:21:16 +0200

    chore: added PR review requests

--- src/test/scala/org/camunda/feel/impl/builtin/BuiltinNumberFunctionTest.scala
+++ src/test/scala/org/camunda/feel/impl/builtin/BuiltinNumberFunctionTest.scala
@@ -219,6 +219,10 @@ class BuiltinNumberFunctionsTest
 
     eval(" odd(5) ") should be(ValBoolean(true))
     eval(" odd(2) ") should be(ValBoolean(false))
+  }
+
+  it should "return true if negative number is odd" in {
+
     eval(" odd(-5)") should be(ValBoolean(true))
     eval(" odd(-2)") should be(ValBoolean(false))
   }
