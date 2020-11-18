f788439d46b1615521bbd019d3deb4dcb9d8185b
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-11-18 14:38:24 +0100

    fix(ObjectContext): respect boolean accessors prepended with "is"
    related to CAM-12577, fixes #212

--- src/test/scala/org/camunda/feel/api/valuemapper/BuiltinValueMapperInputTest.scala
+++ src/test/scala/org/camunda/feel/api/valuemapper/BuiltinValueMapperInputTest.scala
@@ -22,7 +22,7 @@ import java.util.Collections
 import org.camunda.feel.FeelEngine
 import org.camunda.feel.context.Context
 import org.camunda.feel.impl.JavaValueMapper
-import org.camunda.feel.valuemapper.SimpleTestPojo
+import org.camunda.feel.valuemapper.{SimpleBooleanTestPojo, SimpleTestPojo}
 import org.camunda.feel.valuemapper.ValueMapper.CompositeValueMapper
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -123,6 +123,177 @@ class BuiltinValueMapperInputTest extends FlatSpec with Matchers {
       .getOrElse() shouldBe true
   }
 
+  it should "read value from object getter with attribute notation" in {
+
+    val variables = Map("foo" -> new SimpleTestPojo("foo"))
+
+    engine
+      .evalExpression("foo.myString = \"foo\"",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read boolean 'true' from object 'is...' getter" in {
+    val pojo = new SimpleBooleanTestPojo()
+    pojo.setEnabled(true)
+
+    val variables = Map("foo" -> pojo)
+
+    engine
+      .evalExpression("foo.isEnabled() = true",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read boolean 'true' from object getter with attribute notation" in {
+    val pojo = new SimpleBooleanTestPojo()
+    pojo.setEnabled(true)
+
+    val variables = Map("foo" -> pojo)
+
+    engine
+      .evalExpression("foo.enabled = true",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read boolean 'false' from object getter with attribute notation" in {
+    val pojo = new SimpleBooleanTestPojo()
+    pojo.setEnabled(false)
+
+    val variables = Map("foo" -> pojo)
+
+    engine
+      .evalExpression("foo.enabled = false",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read boolean 'true' from object getter" in {
+    val pojo = new SimpleBooleanTestPojo()
+    pojo.setDisabled(true)
+
+    val variables = Map("foo" -> pojo)
+
+    engine
+      .evalExpression("foo.getDisabled() = true",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read boolean 'true' from object getter attribute notation" in {
+    val pojo = new SimpleBooleanTestPojo()
+    pojo.setDisabled(true)
+
+    val variables = Map("foo" -> pojo)
+
+    engine
+      .evalExpression("foo.disabled = true",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read string from object 'is...' getter" in {
+    val pojo = new SimpleBooleanTestPojo()
+    pojo.setFoo("baz")
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.isFoo() = \"baz\"",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "not read string from object 'is...' getter with attribute notation" in {
+    val pojo = new SimpleBooleanTestPojo()
+    pojo.setFoo("baz")
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.foo = \"baz\"",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe ()
+  }
+
+  it should "read scala string from object getter with attribute notation" in {
+    val pojo = new MyScalaType()
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.foo = \"baz\"",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read scala boolean 'true' from object getter" in {
+    val pojo = new MyScalaType()
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.getEnabled() = true",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read scala boolean 'true' from object getter with attribute notation" in {
+    val pojo = new MyScalaType()
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.enabled = true",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read scala boolean 'false' from object IS getter" in {
+    val pojo = new MyScalaType()
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.isDisabled = false",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read scala boolean 'false' from object IS getter with attribute notation" in {
+    val pojo = new MyScalaType()
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.disabled = false",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "read scala string from object IS getter with attribute notation" in {
+    val pojo = new MyScalaType()
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.isBaz = \"foo\"",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe true
+  }
+
+  it should "not read scala string from object IS getter with attribute notation" in {
+    val pojo = new MyScalaType()
+
+    val variables = Map("bar" -> pojo)
+
+    engine
+      .evalExpression("bar.baz = \"foo\"",
+                      context = Context.StaticContext(variables, null))
+      .getOrElse() shouldBe ()
+  }
+
   it should "read java.util.Map" in {
     val map: java.util.Map[java.lang.String, java.lang.Object] =
       new util.HashMap[java.lang.String, java.lang.Object]()
@@ -155,4 +326,15 @@ class BuiltinValueMapperInputTest extends FlatSpec with Matchers {
       .getOrElse() shouldBe true
   }
 
+  class MyScalaType {
+    private val foo: String = "baz"
+    private val baz: String = "foo"
+    private val enabled: Boolean = true
+    private val disabled = false
+
+    def getFoo: String = foo
+    def isBaz: String = baz
+    def getEnabled: Boolean = enabled
+    def isDisabled = disabled
+  }
 }
