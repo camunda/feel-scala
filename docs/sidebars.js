8c7b384b783b32029b46a669d4ebf3e0706d65ea
Philipp Ossler <philipp.ossler@gmail.com>
2022-08-12 10:35:26 +0200

    Tutorial polishing (1) (#496)
    * docs: polish the introduction page
    
    * docs: move the playground out of the tutorial
    
    * style: move the warning on the page
    
    * docs: remove unused tutorial pages
    
    * docs: polish tutorial 1.1
    
    * docs: polish tutorial 1.2
    
    * docs: polish tutorial 1.3
    
    * style: moving note in playground

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -52,25 +52,26 @@ module.exports = {
   ],
   "Tutorial": [
     'tutorial/tutorial',
-    'tutorial/introduction',
     {
-      "Tutorial-1": [
+      "Tutorial 1: Numbers": [
         'tutorial/tutorial-1-1',
         'tutorial/tutorial-1-2',
         'tutorial/tutorial-1-3'
       ],
-      "Tutorial-2": [
+      "Tutorial 2: Strings": [
         'tutorial/tutorial-2-1'
       ],
-      "Tutorial-3": [
+      "Tutorial 3: Temporal": [
          'tutorial/tutorial-3-1'
       ],
-      "Tutorial-4": [
+      "Tutorial 4: Lists": [
          'tutorial/tutorial-4-1'
       ],
     },
-    'tutorial/tutorial-playground',
    ],
+  "Playground": [
+    'playground/playground',
+  ],
   "Changelog": [
       'changelog/changelog'
   ],
