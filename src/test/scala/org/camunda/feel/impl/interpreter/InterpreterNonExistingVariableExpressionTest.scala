a3aff91452419c314a24aa4576082b345f8a6d66
Remco Westerhoud <remco@westerhoud.nl>
2023-07-06 14:28:14 +0200

    test: verify non-existing variables in comparisons
    Adds a few test cases where we do comparisons on non-existing variables.

---
+++ src/test/scala/org/camunda/feel/impl/interpreter/InterpreterNonExistingVariableExpressionTest.scala
@@ -0,0 +1,72 @@
+package org.camunda.feel.impl.interpreter;
+
+import org.camunda.feel.impl.FeelIntegrationTest
+import org.camunda.feel.syntaxtree._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.flatspec.AnyFlatSpec
+
+class InterpreterNonExistingVariableExpressionTest
+        extends AnyFlatSpec
+        with Matchers
+        with FeelIntegrationTest {
+
+  "a non existing variable" should "compare with '='" in {
+    eval("x = 1") should be(ValBoolean(false))
+    eval("1 = x") should be(ValBoolean(false))
+    eval("x = true") should be(ValBoolean(false))
+    eval("true = x") should be(ValBoolean(false))
+    eval(""" x = "string" """) should be(ValBoolean(false))
+    eval(""" "string" = x """) should be(ValBoolean(false))
+    eval("x = null") should be(ValBoolean(true))
+    eval("null = x") should be(ValBoolean(true))
+    eval("x = y") should be(ValBoolean(true))
+  }
+  
+  it should "compare with `<`" in {
+    eval("x < 1") should be(ValNull)
+    eval("1 < x") should be(ValNull)
+    eval("x < true") should be(ValNull)
+    eval("true < x") should be(ValNull)
+    eval(""" x < "string" """) should be(ValNull)
+    eval(""" "string" < x """) should be(ValNull)
+    eval("x < null") should be(ValNull)
+    eval("null < x") should be(ValNull)
+    eval("x < y") should be(ValNull)
+  }
+
+  it should "compare with `>`" in {
+    eval("x > 1") should be(ValNull)
+    eval("1 > x") should be(ValNull)
+    eval("x > true") should be(ValNull)
+    eval("true > x") should be(ValNull)
+    eval(""" x > "string" """) should be(ValNull)
+    eval(""" "string" > x """) should be(ValNull)
+    eval("x > null") should be(ValNull)
+    eval("null > x") should be(ValNull)
+    eval("x > y") should be(ValNull)
+  }
+
+  it should "compare with `<=`" in {
+    eval("x <= 1") should be(ValNull)
+    eval("1 <= x") should be(ValNull)
+    eval("x <= true") should be(ValNull)
+    eval("true <= x") should be(ValNull)
+    eval(""" x <= "string" """) should be(ValNull)
+    eval(""" "string" <= x """) should be(ValNull)
+    eval("x <= null") should be(ValNull)
+    eval("null <= x") should be(ValNull)
+    eval("x <= y") should be(ValNull)
+  }
+
+  it should "compare with `>=`" in {
+    eval("x >= 1") should be(ValNull)
+    eval("1 >= x") should be(ValNull)
+    eval("x >= true") should be(ValNull)
+    eval("true >= x") should be(ValNull)
+    eval(""" x >= "string" """) should be(ValNull)
+    eval(""" "string" >= x """) should be(ValNull)
+    eval("x >= null") should be(ValNull)
+    eval("null >= x") should be(ValNull)
+    eval("x >= y") should be(ValNull)
+  }
+}
