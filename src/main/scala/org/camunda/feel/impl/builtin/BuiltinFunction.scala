ab99978ff4d385bc4a098457b19e4ca9288307bf
Philipp Ossler <philipp.ossler@gmail.com>
2024-08-16 14:23:56 +0200

    fix: Return fatal error in operations
    Adjust all operations to return a fatal error instead of returning null. This is required to limit the access to the special input symbol `?` outside an unary-tests.
    
    Restructure some helper methods to restrict the handling of fatal errors to a few places. This should help to reduce possible problems with fatal errors.
    
    Move some methods to group different aspects together.
    
    (cherry picked from commit c9672081221a1562a704915f6631c900e855706c)

--- src/main/scala/org/camunda/feel/impl/builtin/BuiltinFunction.scala
+++ src/main/scala/org/camunda/feel/impl/builtin/BuiltinFunction.scala
@@ -17,7 +17,7 @@
 package org.camunda.feel.impl.builtin
 
 import org.camunda.feel.logger
-import org.camunda.feel.syntaxtree.{Val, ValError, ValFunction, ValNull}
+import org.camunda.feel.syntaxtree.{Val, ValError, ValFatalError, ValFunction, ValNull}
 
 object BuiltinFunction {
 
@@ -34,12 +34,13 @@ object BuiltinFunction {
   }
 
   private def error: PartialFunction[List[Val], Any] = {
-    case vars if (vars.exists(_.isInstanceOf[ValError])) =>
-      vars.filter(_.isInstanceOf[ValError]).head.asInstanceOf[ValError]
-    case e                                               => {
-      logger.warn(s"Suppressed failure: illegal arguments: $e")
+    case args if args.exists(_.isInstanceOf[ValFatalError]) =>
+      args.find(_.isInstanceOf[ValFatalError])
+    case args if args.exists(_.isInstanceOf[ValError])      => args.find(_.isInstanceOf[ValError])
+    case args                                               =>
+      val argumentList = args.map("'" + _ + "'").mkString(", ")
+      logger.warn(s"Suppressed failure: Illegal arguments: $argumentList")
       ValNull
-    }
   }
 
 }
