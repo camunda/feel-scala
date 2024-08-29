6f284a48aa6c57254c2e850bb34f697cccbf7194
Philipp Ossler <philipp.ossler@gmail.com>
2024-08-29 13:44:38 +0200

    fix: Ignore whitespace in list iteration
    Some whitespace characters were treated as part of the list iteration variable name. As a result, the parsing failed because the keyword "in" was not found.
    
    Fix the parsing issue by excluding whitespace characters as part of the variable name.
    
    (cherry picked from commit 08ef5a4a6d2df3e23418a44d72dffafc3f3f1dbc)

--- src/main/scala/org/camunda/feel/impl/parser/FeelWhitespace.scala
+++ src/main/scala/org/camunda/feel/impl/parser/FeelWhitespace.scala
@@ -29,6 +29,22 @@ import scala.annotation.{switch, tailrec}
   */
 object FeelWhitespace {
 
+  /** Checks if the given character is a whitespace according to FEEL.
+    *
+    * @param char
+    *   the character to check
+    * @return
+    *   true if the given character is a FEEL whitespace
+    */
+  def isWhitespace(char: Char): Boolean = char match {
+    case '\u0009' | '\u0020' | '\u0085' | '\u00A0' | '\u1680' | '\u180E' | '\u2000' | '\u2001' |
+        '\u2002' | '\u2003' | '\u2004' | '\u2005' | '\u2006' | '\u2007' | '\u2008' | '\u2009' |
+        '\u200A' | '\u200B' | '\u2028' | '\u2029' | '\u202F' | '\u205F' | '\u3000' | '\uFEFF' |
+        '\u000A' | '\u000B' | '\u000C' | '\u000D' =>
+      true
+    case _ => false
+  }
+
   implicit object whitespace extends Whitespace {
     def apply(ctx: ParsingRun[_]) = {
       val input                                                    = ctx.input
@@ -51,17 +67,10 @@ object FeelWhitespace {
           (state: @switch) match {
             case 0 =>
               (currentChar: @switch) match {
-                case ' ' | '\t' | '\n' | '\r' | // <-- original
-                    // ==== FEEL specific =====
-                    '\u0009' | '\u0020' | '\u0085' | '\u00A0' | '\u1680' | '\u180E' | '\u2000' |
-                    '\u2001' | '\u2002' | '\u2003' | '\u2004' | '\u2005' | '\u2006' | '\u2007' |
-                    '\u2008' | '\u2009' | '\u200A' | '\u200B' | '\u2028' | '\u2029' | '\u202F' |
-                    '\u205F' | '\u3000' | '\uFEFF' | '\u000A' | '\u000B' | '\u000C' | '\u000D'
-                    // ========================
-                    =>
+                case ' ' | '\t' | '\n' | '\r' | _ if isWhitespace(currentChar) =>
                   rec(current + 1, state)
-                case '/' => rec(current + 1, state = 2)
-                case _   =>
+                case '/'                                                       => rec(current + 1, state = 2)
+                case _                                                         =>
                   if (ctx.verboseFailures) ctx.reportTerminalMsg(current, Msgs.empty)
                   ctx.freshSuccessUnit(current)
               }
